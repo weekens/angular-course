0e6835508e584a30affc67cd3027f66b1a21eb93
weekens <weekens@gmail.com>
2018-01-22 08:49:32 +0300

    WIP on recipe items.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Some description', 'https://www.google.ru/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjS7par7erYAhUFEawKHaZSDnEQjRwIBw&url=https%3A%2F%2Fwww.campbellsoup.co.uk%2Frecipes%2Fvegetarian-pizza&psig=AOvVaw3s9oZWAUnjcZ0qEKA77tqG&ust=1516685856256618')
+  ];
 
   constructor() { }
 
