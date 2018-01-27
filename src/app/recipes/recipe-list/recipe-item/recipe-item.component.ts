8ec922050ed2264a87de79678d5d30f09461a9b2
weekens <weekens@gmail.com>
2018-01-27 08:54:38 +0300

    RecipeItemComponent template draft.

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input()
+  name: string;
+
+  @Input()
+  description: string;
 
   constructor() { }
 
