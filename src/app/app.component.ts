29531b600cbecf70a77b1d3afef1d96258ed9164
weekens <weekens@gmail.com>
2018-04-01 09:17:32 +0300

    Implemented template-driven forms assignment.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  onSubmit(form: NgForm) {
+    console.log('form =', form.value);
+  }
 }
